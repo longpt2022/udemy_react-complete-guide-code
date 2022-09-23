cf6f54c2c23119e9a33730e95354ac4b0a22e6e8
longpt271 <longpt271@gmail.com>
2022-09-23 22:13:29 +0700

    Asm 4: Conditional Content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -69,7 +69,7 @@ const ExpenseForm = props => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    // console.log(expenseData);
 
     props.onSaveExpenseData(expenseData);
 
@@ -111,6 +111,9 @@ const ExpenseForm = props => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>

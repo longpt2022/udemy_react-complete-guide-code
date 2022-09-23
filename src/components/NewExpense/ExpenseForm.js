2c8d0faf0c89361933655731634168e527364313
longpt271 <longpt271@gmail.com>
2022-09-23 17:16:27 +0700

    59. Child-to-Parent Component Communication

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = props => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
@@ -63,23 +63,20 @@ const ExpenseForm = () => {
 
   const submitHandler = event => {
     event.preventDefault();
-    console.log(event);
 
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-
     console.log(expenseData);
 
-    console.log(enteredTitle, enteredAmount, enteredDate);
+    props.onSaveExpenseData(expenseData);
+
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
-    console.log(enteredTitle, enteredAmount, enteredDate);
   };
-  console.log(enteredTitle, enteredAmount, enteredDate);
 
   return (
     <form onSubmit={submitHandler}>

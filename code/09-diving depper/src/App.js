28082a6169620d060fc32d362bf72f912c544846
longpt271 <longpt271@gmail.com>
2022-10-01 22:09:04 +0700

    106. Working with "ref"s

--- code/09-diving depper/src/App.js
+++ code/09-diving depper/src/App.js
@@ -4,17 +4,6 @@ import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
-
-  const addUserHandler = (uName, uAge) => {
-    setUsersList(prevUsersList => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
-
   return (
     <Fragment>
       <AddUser onAddUser={addUserHandler} />

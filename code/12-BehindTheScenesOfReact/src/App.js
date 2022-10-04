436395a1cbffa2ac452e320fb32f83e42210c06e
longpt271 <longpt271@gmail.com>
2022-10-04 17:25:26 +0700

    153. Component Updates In Action

--- code/12-BehindTheScenesOfReact/src/App.js
+++ code/12-BehindTheScenesOfReact/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandle = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandle}>add</Button>
     </div>
   );
 }

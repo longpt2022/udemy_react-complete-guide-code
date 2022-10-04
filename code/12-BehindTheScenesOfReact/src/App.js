515d39f82646c1e646df77b076f2565632a7e168
longpt271 <longpt271@gmail.com>
2022-10-04 18:13:15 +0700

    156. Ngăn Function Re-Creation with useCallback()

--- code/12-BehindTheScenesOfReact/src/App.js
+++ code/12-BehindTheScenesOfReact/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 
@@ -10,9 +10,9 @@ function App() {
 
   console.log('App RUNNING');
 
-  const toggleParagraphHandle = () => {
+  const toggleParagraphHandle = useCallback(() => {
     setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">

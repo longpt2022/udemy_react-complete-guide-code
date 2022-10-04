8dac5ef46bd89c0ea0a125bd369efc458d65a9d1
longpt271 <longpt271@gmail.com>
2022-10-04 14:03:15 +0700

    141. Managing Cart & Modal State

--- code/11-FoodOrderApp/src/components/Layout/HeaderCartButton.js
+++ code/11-FoodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -6,7 +6,7 @@ import CartIcon from '../Cart/CartIcon';
 const HeaderCartButton = props => {
   return (
     <>
-      <button className={classes.button}>
+      <button className={classes.button} onClick={props.onClickCart}>
         <span className={classes.icon}>
           <CartIcon />
         </span>

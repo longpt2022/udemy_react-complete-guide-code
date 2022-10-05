7c77cc39b24db037ac23625d247ca9130e5169ea
longpt271 <longpt271@gmail.com>
2022-10-05 20:34:59 +0700

    182. Preparing The Project For The Next Steps

--- code/14-HTTPS_Request/src/App.js
+++ code/14-HTTPS_Request/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch(
+        'https://react-http-68881-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json'
+      );
       if (!response.ok) {
         throw new Error('Something went wrong! ' + response.status);
       }
@@ -39,20 +41,29 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
+
   if (error) {
     content = <p>{error}</p>;
   }
+
   if (isLoading) {
     content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>

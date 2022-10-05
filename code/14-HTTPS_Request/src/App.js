63e1205f3f2836cb0083ffe4956c91ae6e1cd266
longpt271 <longpt271@gmail.com>
2022-10-05 18:34:38 +0700

    179. Handling Loading & Data States

--- code/14-HTTPS_Request/src/App.js
+++ code/14-HTTPS_Request/src/App.js
@@ -5,9 +5,12 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  console.log(movies);
+  const [isLoading, setIsLoading] = useState(false);
+  // console.log(movies);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
+
     const response = await fetch('https://swapi.dev/api/films');
     const data = await response.json();
 
@@ -20,6 +23,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -28,7 +32,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );

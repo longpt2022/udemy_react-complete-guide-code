6285cfccc2de5a0a4ab60a470c675bfbbd2581dc
longpt271 <longpt271@gmail.com>
2022-10-05 20:13:45 +0700

    181. Using useEffect() For Requests

--- code/14-HTTPS_Request/src/App.js
+++ code/14-HTTPS_Request/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,16 +7,13 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  // console.log(movies);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
       const response = await fetch('https://swapi.dev/api/films');
-      //--- error fetch test
-      // const response = await fetch('https://swapi.dev/api/film');
       if (!response.ok) {
         throw new Error('Something went wrong! ' + response.status);
       }
@@ -36,7 +33,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies.</p>;
 

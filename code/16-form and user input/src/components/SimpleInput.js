ac79c2267f6a6fb5787b2cdb2d4898e2778f16f4
longpt271 <longpt271@gmail.com>
2022-10-06 17:53:31 +0700

    200. Getting User Input Values

--- code/16-form and user input/src/components/SimpleInput.js
+++ code/16-form and user input/src/components/SimpleInput.js
@@ -1,9 +1,43 @@
-const SimpleInput = (props) => {
+import { useState, useRef } from 'react';
+
+const SimpleInput = props => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log('from state:', enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log('from ref:', enteredValue);
+
+    /*
+      - Xóa trường input sau khi nhập bằng ref:
+      KHÔNG NÊN dùng vì sẽ thao tác trực tiếp vào DOM
+      chỉ nên để React thao tác với DOM
+    */
+    // nameInputRef.current.value = '';
+
+    //--- Xóa trường input sau khi nhập bằng state
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>

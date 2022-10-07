63bf669d8ce1f8f62cb294aa65f319af88a304f8
longpt271 <longpt271@gmail.com>
2022-10-07 16:53:10 +0700

    205. Refactoring & Deriving States

--- code/16-form and user input/src/components/SimpleInput.js
+++ code/16-form and user input/src/components/SimpleInput.js
@@ -1,17 +1,13 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
 
 const SimpleInput = props => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      console.log('Name input is valid!');
-    }
-  }, [enteredNameIsValid]);
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
 
+  // 3. On "every keystroke"
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
   };
@@ -19,11 +15,6 @@ const SimpleInput = props => {
   // 2. When a input is "losing focus"
   const nameInputBlurHandler = event => {
     setEnteredNameTouched(true);
-
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
-      return;
-    }
   };
 
   // 1. When a form is "submitted"
@@ -32,22 +23,15 @@ const SimpleInput = props => {
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-    setEnteredNameIsValid(true);
 
     console.log('from state:', enteredName);
 
-    const enteredValue = nameInputRef.current.value;
-    console.log('from ref:', enteredValue);
-
-    // nameInputRef.current.value = '' // DON'T USE THIS ;
     setEnteredName('');
+    setEnteredNameTouched(false);
   };
-
-  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsValid
     ? 'form-control invalid'
@@ -58,7 +42,6 @@ const SimpleInput = props => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}

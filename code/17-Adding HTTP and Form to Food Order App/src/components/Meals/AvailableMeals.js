5e6c69b34f7a03bf52110ad59a2d4564e62d323c
longpt271 <longpt271@gmail.com>
2022-10-07 20:30:39 +0700

    218. Handling Errors

--- code/17-Adding HTTP and Form to Food Order App/src/components/Meals/AvailableMeals.js
+++ code/17-Adding HTTP and Form to Food Order App/src/components/Meals/AvailableMeals.js
@@ -7,12 +7,18 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         'https://react-http-68881-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,13 +35,25 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes['meals-loading']}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes['meals-error']}>
+        <p>{httpError}</p>
       </section>
     );
   }

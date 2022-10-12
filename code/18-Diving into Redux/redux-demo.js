4e6439c4772a14f2f52baf3d189c490d6086712b
longpt271 <longpt271@gmail.com>
2022-10-12 16:00:56 +0700

    231. More Redux Basics

--- code/18-Diving into Redux/redux-demo.js
+++ code/18-Diving into Redux/redux-demo.js
@@ -7,27 +7,36 @@ const redux = require('redux');
 
 //--- 3. tạo hàm reducer, với current state
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    // trả về new state
-    counter: state.counter + 1,
-  };
+  if (action.type === 'increment') {
+    return {
+      // trả về new state
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === 'decrement') {
+    return {
+      // trả về new state
+      counter: state.counter - 1,
+    };
+  }
 };
 
 //--- 2. Tạo store
 // truyền reducer "counterReducer" chịu trách nhiệm thay đổi store
 const store = redux.createStore(counterReducer);
 
-console.log(store.getState());
+// console.log(store.getState());
 
-//--- 4. Tạo 1 subscribe:
+//--- 4. Tạo 1 subscribe: action
 const counterSubscriber = () => {
   const latestState = store.getState();
-  console.log(latestState);
+  console.log('latestState:', latestState);
 };
 
 //--- phương thức subscribe()
 // 'counterSubscriber' sẽ hoạt động bất cứ khi nào dữ liệu và store thay đổi
 store.subscribe(counterSubscriber);
 
-//--- Tạo action
+//--- Gửi đi 1 action
 store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });

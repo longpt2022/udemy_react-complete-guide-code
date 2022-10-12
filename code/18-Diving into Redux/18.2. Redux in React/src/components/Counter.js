5d0a7050cb2d3824cd272f65f60bfdfd25f15632
longpt271 <longpt271@gmail.com>
2022-10-12 17:18:55 +0700

    238. Attaching Payloads to Actions

--- code/18-Diving into Redux/18.2. Redux in React/src/components/Counter.js
+++ code/18-Diving into Redux/18.2. Redux in React/src/components/Counter.js
@@ -11,9 +11,15 @@ const Counter = () => {
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 10 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -22,6 +28,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>

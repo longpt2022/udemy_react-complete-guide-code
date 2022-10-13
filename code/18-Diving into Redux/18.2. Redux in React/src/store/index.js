9fe640c89e080df14dbd4915492b4e910ea9b9af
longpt271 <longpt271@gmail.com>
2022-10-13 19:32:21 +0700

    243. Connecting Redux Toolkit State

--- code/18-Diving into Redux/18.2. Redux in React/src/store/index.js
+++ code/18-Diving into Redux/18.2. Redux in React/src/store/index.js
@@ -1,12 +1,10 @@
 // npm install @reduxjs/toolkit
-// import redux from 'redux';
 
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
@@ -25,45 +23,9 @@ createSlice({
   },
 });
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    // k nên thay đổi state như sau
-    // state.counter++''
-
-    // Chỉ nên overwrite (tạo 1 obj mới) lại như sau
-    // để tránh gặp lỗi sau này
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  // Trả về nếu không có thay đổi gì
-  return state;
-};
-
 // Tạo Redux store
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;

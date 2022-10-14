70dfa61d9874ba59ca3ecfdb4a03da344b497355
longpt271 <longpt271@gmail.com>
2022-10-14 14:35:02 +0700

    274. Using "Switch" and "exact"

--- code/20-SPA with React Router/src/App.js
+++ code/20-SPA with React Router/src/App.js
@@ -1,6 +1,6 @@
 // npm install react-router-dom@5
 
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -12,15 +12,17 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>{' '}
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );

c97cf058ef09073534653936b6ff6cc903988293
longpt271 <longpt271@gmail.com>
2022-10-16 20:44:00 +0700

    start - section 21: Deploying React Apps

---
+++ code/21-Deploying React Apps/src/pages/QuoteDetail.js
@@ -0,0 +1,62 @@
+import { Link, Route, useParams, useRouteMatch } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+
+const QuoteDetail = () => {
+  const params = useParams();
+  const match = useRouteMatch();
+  // console.log(match);
+
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comment
+          </Link>
+        </div>
+      </Route>
+      {/* <Route path="/quotes/:quoteId/comments"></Route> */}
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
+};
+
+export default QuoteDetail;

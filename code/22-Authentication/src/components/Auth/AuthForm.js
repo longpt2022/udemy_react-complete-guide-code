d51b015709b33405c4102e0693c0dbf01f4a1695
longpt271 <longpt271@gmail.com>
2022-10-17 17:01:56 +0700

    306. Showing Feedback To The User

--- code/22-Authentication/src/components/Auth/AuthForm.js
+++ code/22-Authentication/src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin(prevState => !prevState);
@@ -20,6 +21,7 @@ const AuthForm = () => {
 
     // optional: Add validation
 
+    setIsLoading(true);
     if (isLogin) {
     } else {
       fetch(
@@ -34,12 +36,18 @@ const AuthForm = () => {
           headers: { 'Content-Type': 'application/json' },
         }
       ).then(res => {
+        setIsLoading(false);
         if (res.ok) {
           //...
         } else {
           return res.json().then(data => {
-            // show an error modal
-            console.log(data);
+            let errorMessage = 'Authentication failed!';
+
+            // Hiển thị lỗi của firebase trả về
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
           });
         }
       });
@@ -64,7 +72,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
